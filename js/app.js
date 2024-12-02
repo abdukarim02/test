7376876d9cdc6d33a14a531f9bb65257b288e324
abdukarim <166285124+abdukarim02@users.noreply.github.com>
2024-12-02 14:52:59 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// alert('hello');
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 8,
+    spaceBetween: 23,
+    freeMode: true,
+    autoplay: {
+        delay: 500,
+        disableOnInteraction: false,
+      },
+  });
